b39f20eb0e7623551509ac9bf03a451293b995c2
sg823pm <stanislav.guskov@student.tuke.sk>
2024-04-24 02:25:15 +0200

    component shop functionality

--- front-end/src/pages/Main.js
+++ front-end/src/pages/Main.js
@@ -1,7 +1,7 @@
 import './Main.css'
 import {useCookies} from "react-cookie";
-import {useEffect, useRef, useState} from "react";
-import {getProjects, postProject} from "../api/project.service";
+import {useEffect, useState} from "react";
+import {postProject} from "../api/project.service";
 import Projects from "../components/Projects";
 import UserProfilePicture from "../components/UserProfilePicture";
 import {Button, CloseButton} from 'react-bootstrap';
@@ -13,8 +13,17 @@ export default function Main(props) {
     const [showSettingsDropdown, setShowSettingsDropdown] = useState(false);
     const [showSettingsPopup, setShowSettingsPopup] = useState(false);
     const [projectName, setProjectName] = useState("");
+    const [crop, setCrop] = useState();
+    const [isShop, setIsShop] = useState(false);
+
 
     useEffect(() => {
+        const user_settings = cookies.user.user_settings;
+        user_settings.map(setting => {
+            if(setting.hasOwnProperty('crop')) {
+                setCrop(setting.crop);
+            }
+        })
         getProjects();
     }, [projects, projectName, cookies, showProjectAdd]);
 
@@ -80,6 +89,7 @@ export default function Main(props) {
             case "crop-button":
                 user_settings.map(setting => {
                     if(setting.hasOwnProperty('crop')) {
+                        setCrop(setting.crop);
                         setting.crop = setting.crop !== true;
                     }
                 })
@@ -87,6 +97,18 @@ export default function Main(props) {
         console.log(user_settings);
         cookies.user.user_settings = user_settings;
         setCookie("user", cookies.user, { path: "/" });
+    }
+
+    function toggleShop(e) {
+        const button = document.getElementById("component-shop-button");
+        console.log(e.target);
+        if(isShop === false) {
+            setIsShop(true);
+            button.className = "active";
+        } else {
+            setIsShop(false);
+            button.className = "";
+        }
     }
 
     return (
@@ -131,41 +153,82 @@ export default function Main(props) {
                     </div>
                 </div>
                 <div id="shop">
-
+                </div>
+                <div id="other-field">
+                    <h5>Other</h5>
+                    <div id="component-shop-button" onClick={toggleShop}>
+                        <div style={{width: '24px'}}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-basket" viewBox="0 0 16 16">
+                                <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9zM1 7v1h14V7zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5m2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5"/>
+                            </svg>
+                        </div>
+                        <h6>Component Shop</h6>
+                    </div>
                 </div>
             </div>
             <div id="center-right-panel">
                 <div id="upper-center-title">
-
-                </div>
-                <div id="components-column">
-                    <div className="components-row">
-                        <div className="component">
-                            <div className="component-img">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-crop" viewBox="0 0 16 16">
-                                    <path className="bi-inner" d="M3.5.5A.5.5 0 0 1 4 1v13h13a.5.5 0 0 1 0 1h-2v2a.5.5 0 0 1-1 0v-2H3.5a.5.5 0 0 1-.5-.5V4H1a.5.5 0 0 1 0-1h2V1a.5.5 0 0 1 .5-.5m2.5 3a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4H6.5a.5.5 0 0 1-.5-.5"/>
+                    <div id="component-shop-field">
+                        <div id="component-stop-text-area">
+                            {isShop ? (
+                                <>
+                                    <h1 style={{fontSize:'40px'}}>Component Shop</h1>
+                                    <h6>Add any component to your environment setup with just one click!</h6>
+                                </>
+                            ) : (
+                                <>
+                                    <h1 style={{fontSize:'40px'}}>Welcome to designe application!</h1>
+                                </>
+                            )}
+                        </div>
+                        <div id="component-stop-img-area">
+                            <div id="component-shop-icon-background">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="99" height="99" className="bi bi-postcard-heart" id="component-shop-icon" viewBox="0 0 16 16">
+                                    <path d="M8 4.5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0zm3.5.878c1.482-1.42 4.795 1.392 0 4.622-4.795-3.23-1.482-6.043 0-4.622M2.5 5a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1z"/>
+                                    <path fillRule="evenodd" d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1z"/>
                                 </svg>
                             </div>
-                            <div className="component-text">
-                                <h6>Crop function</h6>
-                                <div className="component-button">
-                                    <Button variant="secondary" size="sm" id="crop-button" onClick={toggleComponents}>Add</Button>
-                                </div>
-                            </div>
                         </div>
-                        <div className="component"></div>
-                        <div className="component"></div>
-                        <div className="component"></div>
-                        <div className="component"></div>
-                        <div className="component"></div>
-                        <div className="component"></div>
                     </div>
-                    <div className="components-row">
+                </div>
+                <div id="components-column">
+                    {isShop &&
+                        <>
+                            <div className="components-row">
+                                <div className="component">
+                                    <div className="component-background">
+                                        <div className="component-img">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-crop" viewBox="0 0 16 16">
+                                                <path className="bi-inner" d="M3.5.5A.5.5 0 0 1 4 1v13h13a.5.5 0 0 1 0 1h-2v2a.5.5 0 0 1-1 0v-2H3.5a.5.5 0 0 1-.5-.5V4H1a.5.5 0 0 1 0-1h2V1a.5.5 0 0 1 .5-.5m2.5 3a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4H6.5a.5.5 0 0 1-.5-.5"/>
+                                            </svg>
+                                        </div>
+                                        <div className="component-text">
+                                            <h6>Crop function</h6>
+                                            <div className="component-button">
+                                                {crop ? (
+                                                    <Button variant="secondary" size="sm" id="crop-button" onClick={toggleComponents}>Remove</Button>
+                                                ) : (
+                                                    <Button variant="secondary" size="sm" id="crop-button" onClick={toggleComponents}>Add</Button>
+                                                )}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="component"></div>
+                                <div className="component"></div>
+                                <div className="component"></div>
+                                <div className="component"></div>
+                                <div className="component"></div>
+                                <div className="component"></div>
+                            </div>
+                            <div className="components-row">
 
-                    </div>
-                    <div className="components-row">
+                            </div>
+                            <div className="components-row">
 
-                    </div>
+                            </div>
+                        </>
+                    }
                 </div>
             </div>
             {showProjectAdd && <div id="project-pop-up-dimmed">
